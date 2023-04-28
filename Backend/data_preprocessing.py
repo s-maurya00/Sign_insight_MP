d921ae5919a99ac01a23e6ac99af4d1bcf440c73
Sushil <mauryasushil045@gmail.com>
2023-04-29 01:45:52 +0530

    feat: updated comments

--- Backend/data_preprocessing.py
+++ Backend/data_preprocessing.py
@@ -27,15 +27,15 @@ for action in actions:
 # np.array(sequences).shape
 # (240, 30, 1662)   # If there are 8 actions with 30 videos each then, the shape represents that there are 240 videos with 30 frames and 1662 keypoints
 
-# demo - checking the number of lables for total data
-# np.array(labels).shape    # If there are 8 actions with 30 videos each then, the shape represents that there are 240 videos each with one lable hence there are 240 labels
+# demo - checking the number of labels for total data
+# np.array(labels).shape    # If there are 8 actions with 30 videos each then, the shape represents that there are 240 videos each with one label hence there are 240 labels
 # (240,)
 
 
 # store data as an numpy array for pre-processing
 X = np.array(list_of_all_videos)
 
-# store lables categorically eg. {'A': 0, 'B': 1, 'C': 2} is a dictionary then the value of 'y' will be (90, 3) for 90 videos distributed in 3 folders
+# store labels categorically eg. {'A': 0, 'B': 1, 'C': 2} is a dictionary then the value of 'y' will be (90, 3) for 90 videos distributed in 3 folders
 y = to_categorical(labels).astype(int)
 
 # the value of y will look as:
@@ -48,7 +48,7 @@ y = to_categorical(labels).astype(int)
 #        [0, 0, 0, 0, 0, 0, 0, 1]])
 
 
-# train-test-split takes inputs as: Data, lables, test_size
+# train-test-split takes inputs as: Data, labels, test_size
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.05)
 
 
