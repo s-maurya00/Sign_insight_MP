d6afebf2820d018ef7cff10a1fd5ac286551b83f
Sushil <mauryasushil045@gmail.com>
2023-04-29 00:02:33 +0530

    feat: added try catch blocks in code

--- Backend/data_collection.py
+++ Backend/data_collection.py
@@ -14,6 +14,7 @@ if(cap.isOpened()):
 else:
     print("Error reading the video feed")
 
+
 # Generate directories for each action
 create_dir()
 
@@ -27,22 +28,28 @@ with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=
     for action in actions:
 
         # Loop through each video
-        for curr_video in range(1, no_of_videos + 1):
+        for video_number in range(1, no_of_videos + 1):
 
             # stop capturing data if user presses 'q'
             if should_break:
                 break
 
-            vid_path = str(os.path.join(DATA_PATH, action, str(curr_video)))
-            vid_out = cv2.VideoWriter(vid_path + '.avi', cv2.VideoWriter_fourcc(*'MJPG'), 10, (vid_width, vid_height))
+            try:
+                vid_path = str(os.path.join(DATA_PATH, action, str(video_number)))
+                vid_out = cv2.VideoWriter(vid_path + '.avi', cv2.VideoWriter_fourcc(*'MJPG'), 10, (vid_width, vid_height))
+            except Exception as e:
+                print("Could not save video")
 
             # Loop through frames within each video
             for frame_num in range(1, no_of_frames_in_a_video + 1):
 
                 success, frame = cap.read()
                 
-                # Store the video frame
-                vid_out.write(frame)
+                try:
+                    # Store the video frame
+                    vid_out.write(frame)
+                except:
+                    pass
 
                 # Detect face, hands, pose
                 image, results = mediapipe_detection(frame, holistic)
@@ -54,14 +61,14 @@ with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=
                 if frame_num == 1:
                     cv2.putText(image, 'STARTING COLLECTION', (120,200), 
                                cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255, 0), 4, cv2.LINE_AA)
-                    cv2.putText(image, 'Collecting frames for {} Video Number {}'.format(action, curr_video), (15,12), 
+                    cv2.putText(image, 'Collecting frames for {} Video Number {}'.format(action, video_number), (15,12), 
                                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1, cv2.LINE_AA)
                     # Show to screen
                     cv2.imshow('OpenCV Feed', image)
                     cv2.waitKey(1500)
 
                 else: 
-                    cv2.putText(image, 'Collecting frames for {} Video Number {}'.format(action, curr_video), (15,12), 
+                    cv2.putText(image, 'Collecting frames for {} Video Number {}'.format(action, video_number), (15,12), 
                                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1, cv2.LINE_AA)
                     # Show to screen
                     cv2.imshow('OpenCV Feed', image)
@@ -70,14 +77,17 @@ with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=
                 keypoints = extract_keypoints(results)
 
                 # export keypoints as np.array
-                npy_path = str(os.path.join(DATA_PATH, action, str(curr_video), str(frame_num)))
+                npy_path = str(os.path.join(DATA_PATH, action, str(video_number), str(frame_num)))
                 np.save(npy_path, keypoints)
 
                 # Break gracefully
                 if cv2.waitKey(10) & 0xFF == ord('q'):
                     should_break = True
                     break
-                    
-    cap.release()
-    vid_out.release()
-    cv2.destroyAllWindows()
+
+    try:                    
+        cap.release()
+        vid_out.release()
+        cv2.destroyAllWindows()
+    except:
+        pass
