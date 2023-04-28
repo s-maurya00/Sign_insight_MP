96b3c9ed8eb375422cb613e01815d90755e3de3b
Sushil <mauryasushil045@gmail.com>
2023-04-29 02:41:07 +0530

    feat: moved the program in an callable function

--- Backend/data_collection.py
+++ Backend/data_collection.py
@@ -5,89 +5,94 @@ import cv2
 # My utility functions/constants
 from utility import *
 
-cap = cv2.VideoCapture(0)
 
-# store the camera video while recording the gestures
-if(cap.isOpened()):
-    vid_width = int(cap.get(3))
-    vid_height = int(cap.get(4))
-else:
-    print("Error reading the video feed")
+def start_data_collection():
+    cap = cv2.VideoCapture(0)
 
-
-# Generate directories for each action
-create_dir()
+    # store the camera video while recording the gestures
+    if(cap.isOpened()):
+        vid_width = int(cap.get(3))
+        vid_height = int(cap.get(4))
+    else:
+        print("Error reading the video feed")
 
 
-# Set mediapipe model 
-with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
+    # Generate directories for each action
+    create_dir()
 
-    should_break = False
 
-    # Loop through actions
-    for action in actions:
+    # Set mediapipe model 
+    with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
 
-        # Loop through each video
-        for video_number in range(1, no_of_videos + 1):
+        should_break = False
 
-            # stop capturing data if user presses 'q'
-            if should_break:
-                break
+        # Loop through actions
+        for action in actions:
 
-            try:
-                vid_path = str(os.path.join(DATA_PATH, action, str(video_number)))
-                vid_out = cv2.VideoWriter(vid_path + '.avi', cv2.VideoWriter_fourcc(*'MJPG'), 10, (vid_width, vid_height))
-            except Exception as e:
-                print("Could not save video")
+            # Loop through each video
+            for video_number in range(1, no_of_videos + 1):
 
-            # Loop through frames within each video
-            for frame_num in range(1, no_of_frames_in_a_video + 1):
-
-                success, frame = cap.read()
-                
-                try:
-                    # Store the video frame
-                    vid_out.write(frame)
-                except:
-                    pass
-
-                # Detect face, hands, pose
-                image, results = mediapipe_detection(frame, holistic)
-
-                # Draw landmarks on face, hands, pose
-                draw_styled_landmarks(image, results)
-                
-                # Logic for telling user that a new video is being captured and waiting for 1.5 seconds for user to set his pose  
-                if frame_num == 1:
-                    cv2.putText(image, 'STARTING COLLECTION', (120,200), 
-                               cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255, 0), 4, cv2.LINE_AA)
-                    cv2.putText(image, 'Collecting frames for {} Video Number {}'.format(action, video_number), (15,12), 
-                               cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1, cv2.LINE_AA)
-                    # Show to screen
-                    cv2.imshow('OpenCV Feed', image)
-                    cv2.waitKey(1500)
-
-                else: 
-                    cv2.putText(image, 'Collecting frames for {} Video Number {}'.format(action, video_number), (15,12), 
-                               cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1, cv2.LINE_AA)
-                    # Show to screen
-                    cv2.imshow('OpenCV Feed', image)
-                
-                # get the concatinated keypoints after detection 
-                keypoints = extract_keypoints(results)
-
-                # export keypoints as np.array
-                npy_path = str(os.path.join(DATA_PATH, action, str(video_number), str(frame_num)))
-                np.save(npy_path, keypoints)
-
-                # Break gracefully
-                if cv2.waitKey(10) & 0xFF == ord('q'):
-                    should_break = True
+                # stop capturing data if user presses 'q'
+                if should_break:
                     break
 
-    try:                    
-        cap.release()
-        vid_out.release()
-        cv2.destroyAllWindows()
-    except:
-        pass
+                try:
+                    vid_path = str(os.path.join(DATA_PATH, action, str(video_number)))
+                    vid_out = cv2.VideoWriter(vid_path + '.avi', cv2.VideoWriter_fourcc(*'MJPG'), 10, (vid_width, vid_height))
+                except Exception as e:
+                    print("Could not save video")
+
+                # Loop through frames within each video
+                for frame_num in range(1, no_of_frames_in_a_video + 1):
+
+                    success, frame = cap.read()
+                    
+                    try:
+                        # Store the video frame
+                        vid_out.write(frame)
+                    except:
+                        pass
+
+                    # Detect face, hands, pose
+                    image, results = mediapipe_detection(frame, holistic)
+
+                    # Draw landmarks on face, hands, pose
+                    draw_styled_landmarks(image, results)
+                    
+                    # Logic for telling user that a new video is being captured and waiting for 1.5 seconds for user to set his pose  
+                    if frame_num == 1:
+                        cv2.putText(image, 'STARTING COLLECTION', (120,200), 
+                                cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255, 0), 4, cv2.LINE_AA)
+                        cv2.putText(image, 'Collecting frames for {} Video Number {}'.format(action, video_number), (15,12), 
+                                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1, cv2.LINE_AA)
+                        # Show to screen
+                        cv2.imshow('OpenCV Feed', image)
+                        cv2.waitKey(1500)
+
+                    else: 
+                        cv2.putText(image, 'Collecting frames for {} Video Number {}'.format(action, video_number), (15,12), 
+                                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1, cv2.LINE_AA)
+                        # Show to screen
+                        cv2.imshow('OpenCV Feed', image)
+                    
+                    # get the concatinated keypoints after detection 
+                    keypoints = extract_keypoints(results)
+
+                    # export keypoints as np.array
+                    npy_path = str(os.path.join(DATA_PATH, action, str(video_number), str(frame_num)))
+                    np.save(npy_path, keypoints)
+
+                    # Break gracefully
+                    if cv2.waitKey(10) & 0xFF == ord('q'):
+                        should_break = True
+                        break
+
+        try:                    
+            cap.release()
+            vid_out.release()
+            cv2.destroyAllWindows()
+        except:
+            pass
+
+if(__name__ == '__main__'):
+    start_data_collection()
